a31188a11da5a0dbd7665877275af3d785f1287b
Umka_Ness <melnik.dmitri2012@gmail.com>
2023-05-21 14:48:18 +0200

    update all button

--- my-app/src/components/Game-page/Button.jsx
+++ my-app/src/components/Game-page/Button.jsx
@@ -5,13 +5,13 @@ export const Button = ({ url }) => {
   return (
     <div>
       <form action={url} target="_blank">
-        <button className={css.btn}>
+        <a href={url} className={css.btn}>
           <img
             src={logo}
             alt="Logo"
             style={{ width: "350px", height: "200px" }}
           />
-        </button>
+        </a>
       </form>
     </div>
   );
