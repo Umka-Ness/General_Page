31e4052d988edd6c352bf9f5b529ff4396b58511
Umka_Ness <melnik.dmitri2012@gmail.com>
2023-06-10 03:17:15 +0200

    aaa

--- my-app/src/components/StepByStep/StepByStepButtonGame.jsx
+++ my-app/src/components/StepByStep/StepByStepButtonGame.jsx
@@ -5,7 +5,12 @@ export const StepByStepButtonGame = ({ name, index, id, value }) => {
   const containerRef = useRef(null);
   const refActive = useRef();
   const [isActive, setIsActive] = useState(false);
+  const [xClient, setXclient] = useState(0);
+  const [yClient, setYclient] = useState(0);
 
+  useEffect(() => {
+    console.log(xClient, yClient);
+  }, [xClient, yClient]);
   const handleClick = (e) => {
     console.log(e);
     const container = containerRef.current;
@@ -13,37 +18,34 @@ export const StepByStepButtonGame = ({ name, index, id, value }) => {
 
     if (!isActive) {
       setIsActive(true);
+
       container.style.top = "420px";
-      container.style.transition = "top 1s";
-      refActiveCurrent.style.transform = "scale(2.5)";
+      container.style.transition = "top 0.5s";
+      // refActiveCurrent.style.transform = "scale(2.5)";
       refActiveCurrent.style.zIndex = "9";
       refActiveCurrent.style.position = "absolute";
-      refActiveCurrent.style.top = "50%";
-      refActiveCurrent.style.left = "50%";
-      refActiveCurrent.style.transform = "translate(-50%, -50%)";
-      refActiveCurrent.style.transition = "all 2s";
-      refActiveCurrent.style.transitionTimingFunction =
-        "cubic-bezier(0, 0, 0, 1.2)";
+
+      // clearInterval(interval);
+      // return;
+      refActiveCurrent.style.top = `10%`;
+      refActiveCurrent.style.left = `10%`;
+
+      refActiveCurrent.style.transition = "2s";
       refActiveCurrent.style.width = "900px";
       refActiveCurrent.style.height = "500px";
     } else {
+      refActiveCurrent.style.transform = `translate(10%, 10%)`;
+      container.style.top = "120px";
       refActiveCurrent.style.position = "absolute";
-      refActiveCurrent.style.transitionTimingFunction =
-        "cubic-bezier(1.2, 1.1, 1.1, 1.2)";
-      refActiveCurrent.style.top = "40%";
-      refActiveCurrent.style.left = "60%";
-      refActiveCurrent.style.transform = "translate(-40%, -60%)";
+      refActiveCurrent.style.width = "150px";
+      refActiveCurrent.style.height = "150px";
+      refActiveCurrent.style.transform = "scale(1)";
+      refActiveCurrent.style.zIndex = "1";
       setTimeout(() => {
-        refActiveCurrent.style.transition = "all 0s";
-        container.style.top = "120px";
         refActiveCurrent.style.position = "inherit";
-        refActiveCurrent.style.width = "150px";
-        refActiveCurrent.style.height = "150px";
-        refActiveCurrent.style.transform = "scale(1)";
-        refActiveCurrent.style.zIndex = "1";
-        console.log(12312);
-        setIsActive(false);
       }, 1300);
+      console.log(12312);
+      setIsActive(false);
     }
   };
 
@@ -55,12 +57,16 @@ export const StepByStepButtonGame = ({ name, index, id, value }) => {
         height: "150px",
         border: " 2px solid black",
         borderRadius: "15px",
+        zIndex: "1",
       }}
       onClick={handleClick}
       className={css.btnGame}
       ref={refActive}
       id={id}
     >
+      <button class="zoomTarget" data-targetsize="0.45" data-duration="600">
+        This element zooms when clicked on.
+      </button>
       <div
         ref={containerRef}
         style={{
