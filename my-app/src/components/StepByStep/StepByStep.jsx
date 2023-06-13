38205a4defd31a4c378186fa0c70d181a82b6c39
Umka_Ness <melnik.dmitri2012@gmail.com>
2023-06-13 02:42:11 +0200

    update all

--- my-app/src/components/StepByStep/StepByStep.jsx
+++ my-app/src/components/StepByStep/StepByStep.jsx
@@ -82,160 +82,162 @@ export const StepByStep = () => {
       }
     } else {
       return (
-        <div
-          style={{
-            height: "100vh",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-          }}
-          className={css.wrapper}
-        >
-          <button
-            className={cssPageOne.BackBtn}
-            onClick={handleOnClick}
-            id="back"
-            // style={{ position: "absolute", top: "0" }}
+        <>
+          <div
+            style={{
+              height: "100vh",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+            className={css.wrapper}
           >
-            Back
-          </button>
-          <ul className={css.container}>
-            <li>
-              <div
-                style={{ marginLeft: "50px", maxWidth: "40px" }}
-                id={1}
-                onClick={runGame}
-              >
-                <StepByStepButton name="1" />
-              </div>
-              <div
-                style={{ marginLeft: "-10px", maxWidth: "40px" }}
-                id={2}
-                onClick={runGame}
-              >
-                <StepByStepButton name="2" />
-              </div>
-              <div
-                style={{ marginLeft: "70px", maxWidth: "40px" }}
-                id={3}
-                onClick={runGame}
-              >
-                <StepByStepButton name="3" />
-              </div>
-              <div
-                style={{ marginLeft: "-5px", maxWidth: "40px" }}
-                id={4}
-                onClick={runGame}
-              >
-                <StepByStepButton name="4" />
-              </div>
-            </li>
-            <li>
-              <div
-                style={{
-                  marginLeft: "-120px",
-                  marginTop: "20px",
-                  maxWidth: "40px",
-                }}
-                id={5}
-                onClick={runGame}
-              >
-                <StepByStepButton name="5" />
-              </div>
-              <div
-                style={{
-                  marginLeft: "-100px",
-                  marginBottom: "60px",
-                  maxWidth: "40px",
-                }}
-                id={6}
-                onClick={runGame}
-              >
-                <StepByStepButton name="6" />
-              </div>
-              <div
-                style={{ marginLeft: "-150px", maxWidth: "40px" }}
-                id={7}
-                onClick={runGame}
-              >
-                <StepByStepButton name="7" />
-              </div>
-            </li>
-            <li>
-              <div
-                style={{ marginLeft: "-150px", maxWidth: "40px" }}
-                id={8}
-                onClick={runGame}
-              >
-                <StepByStepButton name="8" />
-              </div>
-              <div
-                style={{ marginLeft: "-100px", maxWidth: "40px" }}
-                id={9}
-                onClick={runGame}
-              >
-                <StepByStepButton name="9" />
-              </div>
-              <div
-                style={{ marginLeft: "-130px", maxWidth: "40px" }}
-                id={10}
-                onClick={runGame}
-              >
-                <StepByStepButton name="10" />
-              </div>
-            </li>
+            <button
+              className={cssPageOne.BackBtn}
+              onClick={handleOnClick}
+              id="back"
+              // style={{ position: "absolute", top: "0" }}
+            >
+              Back
+            </button>
+            <ul className={css.container} style={{ zIndex: "2" }}>
+              <li>
+                <div
+                  style={{ marginLeft: "50px", maxWidth: "40px" }}
+                  id={1}
+                  onClick={runGame}
+                >
+                  <StepByStepButton name="1" />
+                </div>
+                <div
+                  style={{ marginLeft: "-10px", maxWidth: "40px" }}
+                  id={2}
+                  onClick={runGame}
+                >
+                  <StepByStepButton name="2" />
+                </div>
+                <div
+                  style={{ marginLeft: "70px", maxWidth: "40px" }}
+                  id={3}
+                  onClick={runGame}
+                >
+                  <StepByStepButton name="3" />
+                </div>
+                <div
+                  style={{ marginLeft: "-5px", maxWidth: "40px" }}
+                  id={4}
+                  onClick={runGame}
+                >
+                  <StepByStepButton name="4" />
+                </div>
+              </li>
+              <li>
+                <div
+                  style={{
+                    marginLeft: "-120px",
+                    marginTop: "20px",
+                    maxWidth: "40px",
+                  }}
+                  id={5}
+                  onClick={runGame}
+                >
+                  <StepByStepButton name="5" />
+                </div>
+                <div
+                  style={{
+                    marginLeft: "-100px",
+                    marginBottom: "60px",
+                    maxWidth: "40px",
+                  }}
+                  id={6}
+                  onClick={runGame}
+                >
+                  <StepByStepButton name="6" />
+                </div>
+                <div
+                  style={{ marginLeft: "-150px", maxWidth: "40px" }}
+                  id={7}
+                  onClick={runGame}
+                >
+                  <StepByStepButton name="7" />
+                </div>
+              </li>
+              <li>
+                <div
+                  style={{ marginLeft: "-150px", maxWidth: "40px" }}
+                  id={8}
+                  onClick={runGame}
+                >
+                  <StepByStepButton name="8" />
+                </div>
+                <div
+                  style={{ marginLeft: "-100px", maxWidth: "40px" }}
+                  id={9}
+                  onClick={runGame}
+                >
+                  <StepByStepButton name="9" />
+                </div>
+                <div
+                  style={{ marginLeft: "-130px", maxWidth: "40px" }}
+                  id={10}
+                  onClick={runGame}
+                >
+                  <StepByStepButton name="10" />
+                </div>
+              </li>
 
-            <li>
-              <div
-                style={{ marginLeft: "-100px", maxWidth: "40px" }}
-                id={11}
-                onClick={runGame}
-              >
-                <StepByStepButton name="11" />
-              </div>
-              <div
-                style={{ marginLeft: "10px", maxWidth: "40px" }}
-                id={12}
-                onClick={runGame}
-              >
-                <StepByStepButton name="12" />
-              </div>
-              <div
-                style={{ marginLeft: "-10px", maxWidth: "40px" }}
-                id={13}
-                onClick={runGame}
-              >
-                <StepByStepButton name="13" />
-              </div>
-              <div
-                style={{ marginLeft: "-115px", maxWidth: "40px" }}
-                id={14}
-                onClick={runGame}
-              >
-                <StepByStepButton name="14" />
-              </div>
-            </li>
-            <li>
-              <div
-                style={{ marginLeft: "-50px", maxWidth: "40px" }}
-                id={15}
-                onClick={runGame}
-              >
-                <StepByStepButton name="15" />
-              </div>
-              <div>
-                <StepByStepButton name="16" />
-              </div>
-              <div
-                style={{ marginLeft: "-100px", maxWidth: "40px" }}
-                id={16}
-                onClick={runGame}
-              >
-                <StepByStepButton name="17" />
-              </div>
-            </li>
-          </ul>
-        </div>
+              <li>
+                <div
+                  style={{ marginLeft: "-100px", maxWidth: "40px" }}
+                  id={11}
+                  onClick={runGame}
+                >
+                  <StepByStepButton name="11" />
+                </div>
+                <div
+                  style={{ marginLeft: "10px", maxWidth: "40px" }}
+                  id={12}
+                  onClick={runGame}
+                >
+                  <StepByStepButton name="12" />
+                </div>
+                <div
+                  style={{ marginLeft: "-10px", maxWidth: "40px" }}
+                  id={13}
+                  onClick={runGame}
+                >
+                  <StepByStepButton name="13" />
+                </div>
+                <div
+                  style={{ marginLeft: "-115px", maxWidth: "40px" }}
+                  id={14}
+                  onClick={runGame}
+                >
+                  <StepByStepButton name="14" />
+                </div>
+              </li>
+              <li>
+                <div
+                  style={{ marginLeft: "-50px", maxWidth: "40px" }}
+                  id={15}
+                  onClick={runGame}
+                >
+                  <StepByStepButton name="15" />
+                </div>
+                <div>
+                  <StepByStepButton name="16" />
+                </div>
+                <div
+                  style={{ marginLeft: "-100px", maxWidth: "40px" }}
+                  id={16}
+                  onClick={runGame}
+                >
+                  <StepByStepButton name="17" />
+                </div>
+              </li>
+            </ul>
+          </div>
+        </>
       );
     }
   };
--- my-app/src/components/StepByStep/StepByStepButtonGame.jsx
+++ my-app/src/components/StepByStep/StepByStepButtonGame.jsx
@@ -1,13 +1,26 @@
 import React, { useRef, useState } from "react";
 import css from "../StepByStep/StepByStep.module.css";
 
+const positions = {
+  1: { y: 27.5, x: 8, xTranslate: -5, yTranslate: -20 },
+  2: { y: 27.5, x: 24, xTranslate: -25, yTranslate: -20 },
+  3: { y: 27.5, x: 44, xTranslate: -45, yTranslate: -20 },
+  4: { y: 27.5, x: 60, xTranslate: -65, yTranslate: -20 },
+  5: { y: 27.5, x: 76, xTranslate: -75, yTranslate: -20 },
+  6: { y: 51.5, x: 12, xTranslate: -15, yTranslate: -53 },
+  7: { y: 51.5, x: 28, xTranslate: -25, yTranslate: -53 },
+  8: { y: 51.5, x: 44, xTranslate: -45, yTranslate: -53 },
+  9: { y: 51.5, x: 64, xTranslate: -65, yTranslate: -53 },
+  10: { y: 51.5, x: 80, xTranslate: -85, yTranslate: -53 },
+};
+
 export const StepByStepButtonGame = ({ name, index, id, value }) => {
   const containerRef = useRef(null);
   const refActive = useRef();
   const [isActive, setIsActive] = useState(false);
 
-  const handleClick = (e) => {
-    console.log(e);
+  const handleButtonClick = (e) => {
+    const { id } = e.target;
     const container = containerRef.current;
     const refActiveCurrent = refActive.current;
 
@@ -16,70 +29,11 @@ export const StepByStepButtonGame = ({ name, index, id, value }) => {
 
       container.style.top = "420px";
       container.style.transition = "top 0.5s";
-      // refActiveCurrent.style.transform = "scale(2.5)";
+
+      const { y, x, xTranslate, yTranslate } = positions[id];
+
       refActiveCurrent.style.zIndex = "9";
       refActiveCurrent.style.position = "absolute";
-
-      // clearInterval(interval);
-      // return;
-      let x = 0;
-      let y = 0;
-      let xTranslate = 0;
-      let yTranslate = 0;
-      if (e.target.id === "1") {
-        y = 27.5;
-        x = 8;
-        xTranslate = -5;
-        yTranslate = -20;
-        console.log(1);
-      } else if (e.target.id === "2") {
-        y = 27.5;
-        x = 24;
-        xTranslate = -25;
-        yTranslate = -20;
-      } else if (e.target.id === "3") {
-        y = 27.5;
-        x = 44;
-        xTranslate = -45;
-        yTranslate = -20;
-      } else if (e.target.id === "4") {
-        y = 27.5;
-        x = 60;
-        xTranslate = -65;
-        yTranslate = -20;
-      } else if (e.target.id === "5") {
-        y = 27.5;
-        x = 76;
-        xTranslate = -82;
-        yTranslate = -20;
-      } else if (e.target.id === "6") {
-        y = 51.5;
-        x = 12;
-        xTranslate = -15;
-        yTranslate = -53;
-      } else if (e.target.id === "7") {
-        y = 51.5;
-        x = 28;
-        xTranslate = -25;
-        yTranslate = -53;
-      } else if (e.target.id === "8") {
-        y = 51.5;
-        x = 44;
-        xTranslate = -45;
-        yTranslate = -53;
-      } else if (e.target.id === "9") {
-        y = 51.5;
-        x = 64;
-        xTranslate = -65;
-        yTranslate = -53;
-      } else if (e.target.id === "10") {
-        y = 51.5;
-        x = 80;
-        xTranslate = -85;
-        yTranslate = -53;
-      } else {
-        console.log("err");
-      }
       refActiveCurrent.style.top = `${y}%`;
       refActiveCurrent.style.left = `${x}%`;
       refActiveCurrent.style.transform = `translate(${xTranslate}%, ${yTranslate}%)`;
@@ -96,7 +50,6 @@ export const StepByStepButtonGame = ({ name, index, id, value }) => {
         }
       }, 2000);
     } else {
-      // refActiveCurrent.style.transform = `translate(10%, 10%)`;
       container.style.top = "120px";
       refActiveCurrent.style.position = "absolute";
       refActiveCurrent.style.minWidth = "0";
@@ -108,7 +61,6 @@ export const StepByStepButtonGame = ({ name, index, id, value }) => {
       setTimeout(() => {
         refActiveCurrent.style.position = "inherit";
       }, 1300);
-      console.log(12312);
       setIsActive(false);
     }
   };
@@ -119,11 +71,11 @@ export const StepByStepButtonGame = ({ name, index, id, value }) => {
         position: "relative",
         width: "150px",
         height: "150px",
-        border: " 2px solid black",
+        border: "2px solid black",
         borderRadius: "15px",
         zIndex: "1",
       }}
-      onClick={handleClick}
+      onClick={handleButtonClick}
       className={css.btnGame}
       ref={refActive}
       id={id}
@@ -136,7 +88,6 @@ export const StepByStepButtonGame = ({ name, index, id, value }) => {
           left: 0,
           right: 0,
           bottom: 0,
-          // backgroundColor: "red",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
@@ -160,7 +111,6 @@ export const StepByStepButtonGame = ({ name, index, id, value }) => {
           borderRadius: "13px",
         }}
         id={id}
-        // className={css.btnGame}
       >
         <div
           style={{
