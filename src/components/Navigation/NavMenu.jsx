924e5839b46869efeb99fad7b82c96fb6b16a4e3
Umka_Ness <melnik.dmitri2012@gmail.com>
2023-10-12 21:29:21 +0200

    add NavMenu Component

---
+++ src/components/Navigation/NavMenu.jsx
@@ -0,0 +1,101 @@
+import React, { useEffect, useRef, useState } from "react";
+import css from "../../main.module.css";
+import imageQuestionAvatar from "./image/Avatar.png";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { SignOut } from "../SettingAcc/SignOut";
+
+export const NavMenu = () => {
+  const refAvatar = useRef();
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+
+  const auth = getAuth();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // В этом месте пользователь успешно аутентифицирован
+        console.log("Текущий пользователь:", user);
+        // console.log(auth.currentUser.photoURL);
+
+        const userEmail = user.email;
+        const userName = user.displayName;
+        setEmail(userEmail);
+        setName(userName);
+      } else {
+        // Пользователь вышел из системы или не аутентифицирован
+        console.log("Пользователь не в системе.");
+        // console.log(auth.currentUser.photoURL);
+      }
+    });
+
+    return () => unsubscribe(); // Отписка от обновлений при размонтировании компонента
+  }, []);
+  const handleOnClickAvatar = () => {
+    const refAvatarCurrent = refAvatar.current;
+
+    return refAvatarCurrent.classList.toggle(css.isHidden);
+  };
+  return (
+    <>
+      <div className={css.navContainer}>
+        <div>
+          <div style={{ position: "absolute", top: "15px", left: "15px" }}>
+            <div style={{ display: "flex" }}>
+              Langalaxy <p style={{ color: "blue" }}>™</p>
+            </div>
+          </div>
+        </div>
+        <div>awsdwdawd</div>
+        <div
+          style={{
+            position: "relative",
+            height: "45px",
+          }}
+        >
+          <img
+            src={imageQuestionAvatar}
+            alt="img"
+            className={css.avatarImg}
+            onClick={handleOnClickAvatar}
+          />
+
+          <ul
+            className={`${css.toggleMenu} ${css.nothing} ${css.isHidden}`}
+            ref={refAvatar}
+          >
+            <div>
+              <li
+                style={{
+                  marginTop: "10px",
+                  marginBottom: "5px",
+                  color: "#909196",
+                  cursor: "default",
+                }}
+              >
+                {name ? name : null}
+              </li>
+              <li
+                style={{
+                  marginBottom: "10px",
+                  color: "#909196",
+                  cursor: "default",
+                }}
+              >
+                {email}
+              </li>
+              <li className={css.toggleMenuList}>Profile</li>
+              <li className={css.toggleMenuList}>Nothing2</li>
+            </div>
+            <li className={css.toggleMenuList} id={"5"}>
+              Setting
+            </li>
+            <li className={css.toggleMenuList}>
+              <SignOut />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/components/Navigation/NavigationBtn.jsx
+++ src/components/Navigation/NavigationBtn.jsx
@@ -5,45 +5,19 @@ import { BtnForNavigation } from "./BtnForNavigation";
 import { Lecture } from "../Lecture/Lecture";
 import { Setting } from "../SettingAcc/Setting";
 import css from "../../main.module.css";
-import imageQuestionAvatar from "./image/Avatar.png";
 
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { SignOut } from "../SettingAcc/SignOut";
 import { collection, getDocs, getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import firebaseConfig from "../../firebase";
+import { NavMenu } from "./NavMenu";
 
 export const NavigationBtn = () => {
   const [selectedId, setSelectedId] = useState("");
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const refAvatar = useRef();
 
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
-
-  const auth = getAuth();
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // В этом месте пользователь успешно аутентифицирован
-        console.log("Текущий пользователь:", user);
-        // console.log(auth.currentUser.photoURL);
-
-        const userEmail = user.email;
-        const userName = user.displayName;
-        setEmail(userEmail);
-        setName(userName);
-      } else {
-        // Пользователь вышел из системы или не аутентифицирован
-        console.log("Пользователь не в системе.");
-        // console.log(auth.currentUser.photoURL);
-      }
-    });
-
-    return () => unsubscribe(); // Отписка от обновлений при размонтировании компонента
-  }, []);
 
   const fetchDataAsync = async () => {
     const querySnapshot = await getDocs(collection(db, "users"));
@@ -72,11 +46,6 @@ export const NavigationBtn = () => {
     const id = e.target.id;
     setSelectedId(id);
   };
-  const handleOnClickAvatar = () => {
-    const refAvatarCurrent = refAvatar.current;
-
-    return refAvatarCurrent.classList.toggle(css.isHidden);
-  };
 
   const renderContent = () => {
     if (selectedId === "1") {
@@ -99,77 +68,7 @@ export const NavigationBtn = () => {
             }}
           >
             <div style={{ display: "flex" }}>
-              <div
-                style={{
-                  display: "flex",
-                  width: "100%",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                  position: "absolute",
-                  top: "0",
-                  height: "50px",
-                  borderBottom: "2px solid black",
-                }}
-              >
-                <div>
-                  <div
-                    style={{ position: "absolute", top: "15px", left: "15px" }}
-                  >
-                    <div style={{ display: "flex" }}>
-                      Langalaxy <p style={{ color: "blue" }}>™</p>
-                    </div>
-                  </div>
-                </div>
-                <div>awsdwdawd</div>
-                <div
-                  style={{
-                    position: "relative",
-                    height: "45px",
-                  }}
-                >
-                  <img
-                    src={imageQuestionAvatar}
-                    alt="img"
-                    className={css.avatarImg}
-                    onClick={handleOnClickAvatar}
-                  />
-
-                  <ul
-                    className={`${css.toggleMenu} ${css.nothing} ${css.isHidden}`}
-                    ref={refAvatar}
-                  >
-                    <div>
-                      <li
-                        style={{
-                          marginTop: "10px",
-                          marginBottom: "5px",
-                          color: "#909196",
-                          cursor: "default",
-                        }}
-                      >
-                        {name ? name : null}
-                      </li>
-                      <li
-                        style={{
-                          marginBottom: "10px",
-                          color: "#909196",
-                          cursor: "default",
-                        }}
-                      >
-                        {email}
-                      </li>
-                      <li className={css.toggleMenuList}>Profile</li>
-                      <li className={css.toggleMenuList}>Nothing2</li>
-                    </div>
-                    <li className={css.toggleMenuList} id={"5"}>
-                      Setting
-                    </li>
-                    <li className={css.toggleMenuList}>
-                      <SignOut />
-                    </li>
-                  </ul>
-                </div>
-              </div>
+              <NavMenu />
               <div
                 style={{
                   display: "flex",
