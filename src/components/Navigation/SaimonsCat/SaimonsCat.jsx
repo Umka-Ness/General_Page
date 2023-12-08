a2267cd47e767e7e76693df057a926b728d5b532
Umka_Ness <melnik.dmitri2012@gmail.com>
2023-12-08 15:05:04 +0100

    add video simons cat

--- src/components/Navigation/SaimonsCat/SaimonsCat.jsx
+++ src/components/Navigation/SaimonsCat/SaimonsCat.jsx
@@ -4,6 +4,12 @@ import video1 from "./Video/one.mp4";
 import video2 from "./Video/2.mp4";
 import video3 from "./Video/3.mp4";
 import video4 from "./Video/4.mp4";
+import video5 from "./Video/5.mp4";
+import video6 from "./Video/6.mp4";
+import video7 from "./Video/7.mp4";
+import video8 from "./Video/8.mp4";
+import video9 from "./Video/9.mp4";
+import video10 from "./Video/10.mp4";
 
 import { Materials } from "../Materials";
 
@@ -38,6 +44,7 @@ export const SaimonsCat = () => {
               alignItems: "center",
               gap: "20px",
               flexWrap: "wrap",
+              paddingTop: "50px",
             }}
           >
             <video
@@ -45,8 +52,8 @@ export const SaimonsCat = () => {
               autoPlay="true"
               controls
               style={{
-                width: "150px",
-                height: "150px",
+                width: "80%",
+                height: "30%",
                 border: "2px solid black",
               }}
             ></video>
@@ -54,8 +61,8 @@ export const SaimonsCat = () => {
               src={video2}
               controls
               style={{
-                width: "150px",
-                height: "150px",
+                width: "80%",
+                height: "30%",
                 border: "2px solid black",
               }}
             ></video>
@@ -63,8 +70,8 @@ export const SaimonsCat = () => {
               src={video3}
               controls
               style={{
-                width: "150px",
-                height: "150px",
+                width: "80%",
+                height: "30%",
                 border: "2px solid black",
               }}
             ></video>
@@ -72,8 +79,62 @@ export const SaimonsCat = () => {
               src={video4}
               controls
               style={{
-                width: "150px",
-                height: "150px",
+                width: "80%",
+                height: "30%",
+                border: "2px solid black",
+              }}
+            ></video>
+            <video
+              src={video5}
+              controls
+              style={{
+                width: "80%",
+                height: "30%",
+                border: "2px solid black",
+              }}
+            ></video>
+            <video
+              src={video6}
+              controls
+              style={{
+                width: "80%",
+                height: "30%",
+                border: "2px solid black",
+              }}
+            ></video>
+            <video
+              src={video7}
+              controls
+              style={{
+                width: "80%",
+                height: "30%",
+                border: "2px solid black",
+              }}
+            ></video>
+            <video
+              src={video8}
+              controls
+              style={{
+                width: "80%",
+                height: "30%",
+                border: "2px solid black",
+              }}
+            ></video>
+            <video
+              src={video9}
+              controls
+              style={{
+                width: "80%",
+                height: "30%",
+                border: "2px solid black",
+              }}
+            ></video>
+            <video
+              src={video10}
+              controls
+              style={{
+                width: "80%",
+                height: "30%",
                 border: "2px solid black",
               }}
             ></video>
